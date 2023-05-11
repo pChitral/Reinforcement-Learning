cac76e660c43d9196969a478ab5f943400bf605d
pChitral <chitral.patil@vit.edu.in>
2023-05-11 19:18:41 -0400

    added button

--- app.py
+++ app.py
@@ -148,38 +148,38 @@ def app():
     done = False
     reward = 0
     count = 1
+    if st.button(":running: Lets Go!",  type="secondary", disabled=False, use_container_width=False):
+    
+        while not done:
+            # Get the agent's action
+            action = agent.act(obs, reward, done)
 
-    # Run the simulation
-    while not done:
-        # Get the agent's action
-        action = agent.act(obs, reward, done)
+            # Take the action and get the next observation and reward
+            next_obs, reward, done, _ = env.step(action)
+            if done:
+                break
 
-        # Take the action and get the next observation and reward
-        next_obs, reward, done, _ = env.step(action)
-        if done:
-            break
+            # Get the next action
+            next_action = agent.act(next_obs, reward, done)
 
-        # Get the next action
-        next_action = agent.act(next_obs, reward, done)
+            # Update the Q-table
+            agent.learn(obs, action, reward, next_obs, next_action, done)
 
-        # Update the Q-table
-        agent.learn(obs, action, reward, next_obs, next_action, done)
+            # Update the current observation
+            obs = next_obs
 
-        # Update the current observation
-        obs = next_obs
+            # Display the action, reward, and rendering
+            st.write(f"For the {count}th step")
+            st.write('Action:', action)
+            st.write('Reward:', reward)
+            st.write('Done:', done)
+            env.render()
+            st.pyplot()
 
-        # Display the action, reward, and rendering
-        st.write(f"For the {count}th step")
-        st.write('Action:', action)
-        st.write('Reward:', reward)
-        st.write('Done:', done)
-        env.render()
-        st.pyplot()
-
-        count += 1
-    if count>1: 
-        st.balloons()
-        st.success('We have reached our treasure!', icon="✅")
+            count += 1
+        if count>1: 
+            st.balloons()
+            st.success('We have reached our treasure!', icon="✅")
 
 # Run the app
 if __name__ == '__main__':
